48508678a7f7b499bf87d1f6e65fec4a5deac2ab
Lam <shun_min@hotmail.com>
2023-03-05 00:18:20 +0800

    HTW-1: Setup basic url and views

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:event_pk>/', views.detail, name="detail"),
+    # path('<int:person_id>', views.person, name="person"),
 ]
