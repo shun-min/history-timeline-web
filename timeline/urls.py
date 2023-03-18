945c4e8227c279e164239333b9122ec92ace27db
Lam <shun_min@hotmail.com>
2023-03-18 16:38:04 +0800

    Hotfix: Fix item url to detail page

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = "timeline"
 urlpatterns = [
     path('', views.index, name="index"),
     path('<int:event_pk>/', views.detail, name="detail"),
